4ab276ad0e1ada2ee1deebfc324c95e10fdc6def
avanikatyayan <avanikatyayan@gmail.com>
2021-08-10 15:34:18 +0530

    books

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,29 +1,39 @@
 import React from 'react'
 import './index.css'
+const FirstBook={
+    title :"IKIGAI",
+    author :"Hector Gracia",
+    img :"https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg"
+}
+const SecondBook={
+    title :"The Psychology of Money",
+    author :"Morgan Housel",
+    img :"https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg"
+}
+const ThirdBook={
+    title :"The Alchemist",
+    author :"Paulo Coelho",
+    img :"https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg"
+}
+
 
 function Bookstore() {
     return (
         <section className ="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            
+        <Book img={FirstBook.img} title={FirstBook.title} author={FirstBook.author} />
+        <Book img={SecondBook.img} title={SecondBook.title} author={SecondBook.author} />  
+        <Book img={ThirdBook.img} title={ThirdBook.title} author={ThirdBook.author} />
         </section>
     )
 }
-const Book = () =>{
-    return <article>
-        <Img />
-        <Title />
-        <Author />
+const Book = (props) =>{
+    return <article className='book'>
+        <img src={props.img} />
+        <h1>{props.title}</h1>
+        <h3>{props.author}</h3>
         
     </article>
 }
-const Img = () => <img src = "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg" alt="IKIGAI"></img>
-const Title = () => <h1 style={{color:'Navy', fontSize:'0.95', marginTop:'0.25rem'}}>IKIGAI</h1>
-const Author = () => <h3 style={{color:'#617d98', fontSize:'0.75', marginTop:'0.25rem'}}>Hector Gracia</h3>
+ 
 
 export default Bookstore
