07049a8c645458db5e010c00a5ae2301f49741db
avanikatyayan <avanikatyayan@gmail.com>
2021-08-10 13:23:46 +0530

    BS

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,22 +1,29 @@
 import React from 'react'
+import './index.css'
 
 function Bookstore() {
     return (
-        <section>
+        <section className ="booklist">
             <Book />
             <Book />
             <Book />
             <Book />
             <Book />
             <Book />
+            
         </section>
     )
 }
 const Book = () =>{
     return <article>
-        <Img></Img>
+        <Img />
+        <Title />
+        <Author />
+        
     </article>
 }
 const Img = () => <img src = "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg" alt="IKIGAI"></img>
+const Title = () => <h3>IKIGAI</h3>
+const Author = () => <h1>Hector Gracia</h1>
 
 export default Bookstore
