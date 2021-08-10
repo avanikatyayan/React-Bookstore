87e4b1cde7d9fa5c1ca8363f5a2d28388dc5ba38
avanikatyayan <avanikatyayan@gmail.com>
2021-08-10 11:07:22 +0530

    second

---
+++ src/SecondComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render} from "react-dom";
+
+function SecondComponent() {
+    return (
+        <React.Fragment>
+        <div>
+            <h1>Avani</h1>
+        </div>
+
+        </React.Fragment>
+        
+    )
+}
+
+export default SecondComponent
